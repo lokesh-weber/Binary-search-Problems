923cf4aa984fa679c544280fdbb46e52a2d092fd
Lokesh_Sanodiya <sanodiyalokesh335@gmail.com>
2022-01-28 13:35:44 +0530

    Binary search problems

---
+++ src/com/lokesh/FindCeiling.java
@@ -0,0 +1,40 @@
+package com.lokesh;
+
+public class FindCeiling {
+    public static void main(String[] args) {
+        int []arr = {12,23,33,44,55,};
+        int target = 21;
+        int ans = ceiling(arr,target);
+        System.out.println(ans);
+    }
+    static int ceiling(int [] arr, int target) {
+        int start = 0;
+        int end = arr.length-1;
+        boolean ascending = false;
+        if(arr[start] < arr[end]) {
+            ascending = true;
+        }
+        if(ascending){
+        while(start <= end) {
+            int mid = start + (end - start) / 2;
+            if (target == arr[mid]) {
+                return arr[mid];
+            } else if (target > arr[mid]) {
+                start = mid + 1;
+            } else
+                end = mid - 1;
+            }
+        }
+        else
+            while(start <= end) {
+                int mid = start + (end - start) / 2;
+                if (target == arr[mid]) {
+                    return arr[mid];
+                } else if (target < arr[mid]) {
+                    start = mid + 1;
+                } else
+                    end = mid - 1;
+            }
+            return arr[start];
+    }
+}
